5b1c212f9c97982b861d2a3a68e26ef6c7b60fb2
Stephanie Xue <stephaniexue3@gmail.com>
2024-08-23 16:18:31 -0700

    fix nav bar scrolling effect

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,12 +2,18 @@ import React from "react"
 
 function Contact() {   
   return (
-    <div>
-      <h1>Contact Me</h1>
-      <p>
-        Email:
-        <a href="mailto:   ">   </a>
-        </p>
+    <div classname="contact-page">
+      <h1 className="contact-title">Contact Me.</h1>
+      <p className="contact-description">
+        If you'd like to get in touch, feel free to send me an email or connect with me on LinkedIn, Github, or Instagram. I'm always open to discussing new projects and networking with like-minded individuals.
+      </p>
+      <img className="contact-image" src="/images/contact.png" alt="contact" />
+      <div className="contact-buttons">
+        <button className="contact-button">Email: stephaniexue3@gmail.com</button>
+        <button className="contact-button">Github</button>
+        <button className="contact-button">Linkedin</button>
+        <button className="contact-button">Instagram</button>
+      </div>
     </div>
     );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,9 +2,30 @@ import React from "react"
 import { NavLink } from "react-router-dom"
 
 function NavBar() {
+
+    // Set the initial state of the navbar to be inactive when not scrolling (not highlighted)
+    const [navbarActive, setNavbarActive] = React.useState(false);
+
+    // Function to change the navbar based on the user's scroll position
+    function changeNavbar() {
+        if (window.scrollY > 0) {
+            setNavbarActive(true);
+        } else {
+            setNavbarActive(false);
+        }
+    }
+
+    // Add an event listener to the window object to listen for scroll events to change the navbar
+    React.useEffect(() => {
+        window.addEventListener('scroll', changeNavbar);
+        return () => {
+            window.removeEventListener('scroll', changeNavbar);
+        };
+    }, []);
+
     return (
-        <nav className="navbar">
-            <ul className="navbar-list">
+        <nav className="navbar" >
+            <ul className={navbarActive ? "navbar-list navbar-list-active" : "navbar-list"}>
                 <li className="navbar-list-item">
                     <NavLink 
                         className={({ isActive }) => isActive ? "navbar-list-link nav-active" : "navbar-list-link"} 
