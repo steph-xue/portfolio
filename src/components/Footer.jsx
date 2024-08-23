06b215416b5316f13db0ee31b450b290ac5a4256
Stephanie Xue <stephaniexue3@gmail.com>
2024-08-23 13:52:12 -0700

    add icon links to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,28 @@
 import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 function Footer() {
     return (
         <footer className="footer">
-            <p>© 2024 - Stephanie Xue</p>
+            <p className="copyright">© 2024 - Stephanie Xue</p>
+            <div className="icons">
+                <a className="icon-link" href="https://github.com/steph-xue">
+                    <FontAwesomeIcon className="icon" icon={faGithub} />
+                </a>
+                <a className="icon-link" href="https://www.linkedin.com/in/stephanie-x-639182188/">
+                    <FontAwesomeIcon className="icon" icon={faLinkedin} />
+                </a>
+                <a className="icon-link" href="https://www.instagram.com/stephxue_">
+                    <FontAwesomeIcon className="icon" icon={faInstagram} />
+                </a>
+                <a className="icon-link" href="mailto:stephaniexue3@gmail.com">
+                    <FontAwesomeIcon className="icon" icon={faEnvelope} />
+                </a>
+            </div>
             
         </footer>
     );
