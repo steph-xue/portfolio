04c4bc44902197ec50188930f660f7bc075cf744
Stephanie Xue <stephaniexue3@gmail.com>
2024-08-29 17:01:56 -0700

    add healthcare data

---
+++ src/experiences-data/healthcare-data.jsx
@@ -0,0 +1,98 @@
+export default [
+    {
+        id: 1,
+        name: "superstore-intern",
+        title: "Real Canadian Superstore - Community Pharmacy Intern",
+        location: "4651 No. 3 Rd, Richmond, BC, Canada",
+        dates: "May 2019 - May 2019",
+        description: [
+            "Utilized Kroll and PharmaNet pharmacy software for processing prescriptions, assisted with telephone and in-person inquiries, counselled patients on their medications, and liaised with physicians and other health care professionals to resolve prescription-related issues and improve patient medication management in a fast-paced pharmacy environment"
+        ]
+    },
+    {
+        id: 2,
+        name: "london-drugs-assistant",
+        title: "London Drugs #74 – Pharmacy Assistant",
+        location: "2230 West Broadway, Vancouver, BC, Canada",
+        dates: "Aug 2019 – Jan 2020",
+        description: [
+            "» Used Kroll and PharmaNet pharmacy software for processing prescriptions, answered telephone and in-person patient inquiries, counselled patients on medications, collaborated with health care professionals including physicians to resolve drug therapy issues, and ordered stock through manufacturers like McKesson and Imperial, in a fast-paced pharmacy setting"
+        ]
+    },
+    {
+        id: 3,
+        name: "rexall-intern",
+        title: "Rexall #7174 & #7159 – Community Pharmacy Intern",
+        location: "499 Granville St & 6580 Fraser St, Vancouver, BC, Canada",
+        dates: "May 2021 – Jun 2021, Sept 2021 – Oct 2021",
+        description: [
+            "Actively led COVID-19 Pfizer, AstraZeneca, and influenza immunization clinics to help vaccinate 1200+ patients. Duties involved managing appointment scheduling, screening for contraindications, and administering vaccines using MedMe Health software",
+            "Conducted medication reviews and created comprehensive medication management plans to optimize drug therapy",
+            "Used Nexxsys and PharmaNet pharmacy software for processing prescriptions, answered telephone and in-person patient inquiries, counselled patients on medications, and collaborated with health care professionals to optimize patient outcomes"
+        ]
+    },
+    {
+        id: 4,
+        name: "vgh-intern",
+        title: "Vancouver General Hospital Atrial Fibrillation & Heart Function Clinic – Outpatient Pharmacy Intern",
+        location: "2775 Laurel St, Vancouver, BC, Canada",
+        dates: "Jan 2022 – Feb 2022",
+        description: [
+            "Assessed complex cardiac patients using patient records from CareConnect and the iClinic MD electronic health record ",
+            "Assisted with atrial fibrillation and heart failure clinic visits by performing medication reconciliation to optimize medication management, presenting recommendations to cardiologists during interprofessional rounds with clinical reasoning, conducting clinical consults, performing telehealth follow-ups, and titrating medications to optimize therapy",
+            "Educated patients on sick day medication management strategies, heart failure action plans to help monitor and respond to changes in symptoms, and electrical cardioversion instructions regarding drug therapies to hold before procedures",
+            "Developed educational initiatives including a nursing in-service to teach hospital wards about inhalers using brochures and simulated device demonstrations, with a focus on new advancements in therapy and inhaler delivery systems",
+            "Presented a case presentation to clinicians discussing the evidence behind using dual antiplatelet therapy after an acute coronary syndrome in patients treated with coronary artery bypass graft surgery",
+            "Created an educational infographic on the use of SGLT2 inhibitors in patients with heart failure, with a focus on its short and long-term effects on kidney function, as well as reduction in heart failure hospitalizations and cardiovascular death"
+        ]
+    },
+    {
+        id: 5,
+        name: "bc-children-intern",
+        title: "BC Children’s Hospital (General Surgery & Neurology) – Inpatient Pharmacy Intern",
+        location: "4500 Oak St, Vancouver, BC, Canada",
+        dates: "Mar 2022 – Apr 2022",
+        description: [
+            "Assessed complex pediatric patients in the general surgery and neurology wards using patient records from CareConnect and the Clinical and Systems Transformation (CST) Cerner electronic health record ",
+            "Developed comprehensive patient-centred care plans to optimize clinical outcomes, attended interprofessional rounds, presented pharmaceutical recommendations to physicians, engaged in clinical topic discussions, and counselled families on the appropriate use of newly prescribed medications in addition to other pertinent instructions before discharge",
+            "Presented two case presentations to clinicians, discussing the most optimal duration of antibiotics for the treatment of subdural empyema secondary to bacterial meningitis in infants, and examining the evidence for vitamins and minerals in pediatric burn patients"
+        ]
+    },
+    {
+        id: 6,
+        name: "shoppers-intern-assistant",
+        title: "Shoppers Drug Mart #2275 – Community Pharmacy Intern & Pharmacy Assistant",
+        location: "1202 West Pender St, Vancouver, BC, Canada",
+        dates: "May 2022 – Jun 2022 ",
+        description: [
+            "Developed an educational initiative on the BC smoking cessation program using informative brochures and infographics",
+            "Performed clinical consults, conducted medication review services to enhance patients’ understanding of their medications, created comprehensive medication management plans to optimize drug therapy, and administered vaccines",
+            "Utilized Healthwatch and PharmaNet pharmacy software for processing prescriptions, attended to telephone and in-person patient inquiries, counselled patients on medications, consulted with physicians and other health care professionals to resolve prescription-related issues and optimize treatment outcomes, ordered medications through manufacturers like McKesson and Imperial, and helped with other pharmacy related duties in a fast-paced environment"
+        ]
+    },
+    {
+        id: 7,
+        name: "medical-center-rx",
+        title: "Medical Centre Prescriptions – Pharmacist (Mental Health & Diabetes)",
+        location: "10225 King George Blvd, Surrey, BC, Canada	",
+        dates: "Jul 2022 – Oct 2023",
+        description: [
+            "Actively managed hospital discharges in coordination with clinicians and some of the largest mental health teams in BC (Surrey Mental Health, Delta Mental Health, ACT teams, ICM teams, etc.) to help with recovery-oriented community reintegration, through the arrangement of blister pack services and daily witnessed nurse dispense",
+            "Performed innovative point-of-care finger blood tests for clozapine patients using CSAN and OLO devices, which use imaging technology and artificial intelligence to quantitatively determine white blood cell and neutrophil counts",
+            "Closely monitored patients on clozapine using Excel, as one of the leading clozapine dispensing pharmacies in BC",
+            "Actively engaged in clinical pharmacy services, including patient consults to prescribe for minor ailments, medication reviews to optimize drug therapy, and the administration of long-acting antipsychotic injections and other vaccinations",
+            "Used Kroll and PharmaNet pharmacy software for processing prescriptions, answered telephone and in-person patient inquiries, counselled on medications, and ordered stock through manufacturers like Kohl and Frisch, McKesson, and uniPHARM"
+        ]
+    },
+    {
+        id: 8,
+        name: "shoppers-burnaby",
+        title: "Shoppers Drug Mart #2127 – Pharmacist ",
+        location: "4303 Hastings Street, Burnaby, BC, Canada",
+        dates: "Mar 2024 – Present",
+        description: [
+            "Actively engaged in clinical consults, prescribed for minor ailments after appropriate patient assessment, conducted medication review services to enhance patients’ understanding of their medications, and administered vaccinations",
+            "Utilized Healthwatch pharmacy software for prescription data entry, assisted with telephone and in-person patient inquiries, counselled on medications, and collaborated with healthcare professionals to improve patient care outcomes "
+        ]
+    },
+]
