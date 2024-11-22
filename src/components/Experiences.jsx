fb1da68d2c285a1a017720d7af365b07a0d0e989
Stephanie Xue <stephaniexue3@gmail.com>
2024-11-21 23:17:28 -0800

    update technical skills

--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -93,7 +93,7 @@ function Experiences(props) {
                     </div>
                     <div className="skills-category skills-development">
                         <h3 className="skills-category-title">Development Practices/Tools</h3>
-                        <p className="skills-category-items">Git, Github Copilot, Visual Studio Code</p>
+                        <p className="skills-category-items">Git, Github Copilot, VS Code</p>
                     </div>
                     <div className="skills-category skills-storage">
                         <h3 className="skills-category-title">Databases</h3>
