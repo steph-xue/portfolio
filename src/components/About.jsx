67b24f3a58e3990120f6fdc2ed7ec627c3c1723e
Stephanie Xue <stephxue@students.cs.ubc.ca>
2025-04-23 01:18:28 -0700

    update primary care clinic project

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -70,7 +70,7 @@ function About(props) {
             <div className="about-container-1">
                 <img src="/images/profile-pic.png" alt="profile-pic" className="profile-image" />
                 <div className="about-container-1-text">
-                    <h2 className="about-subtitle">I'm a currently a UBC Bachelor of Computer Science (BCS) student based in Burnaby, BC, Canada.</h2>
+                    <h2 className="about-subtitle">I'm a currently a UBC Bachelor of Computer Science (BCS) student based in Vancouver, BC, Canada.</h2>
                     <p className="about-description">
                         As a UBC Bachelor of Computer Science (BCS) student with previous background in healthcare as a pharmacist,
                         I have a strong enthusiasm for technology, software development, UX/UI design, artificial intelligence,
