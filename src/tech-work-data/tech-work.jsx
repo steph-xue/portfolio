75fb718c9bb9b14a5cb0f7d2d774cd7b728e9b9e
Stephanie Xue <stephaniexue3@gmail.com>
2025-11-04 20:37:27 -0800

    update kardium work experience

--- src/tech-work-data/tech-work.jsx
+++ src/tech-work-data/tech-work.jsx
@@ -2,13 +2,17 @@ export default [
     {
         id: 1,
         name: "kardium",
-        title: "Kardium – Medical Device Software Developer Intern",
+        title: "Kardium – Medical Device Software UI Developer Intern",
         location: "Burnaby, BC, Canada (Hybrid)",
         dates: "Sept 2025 – Present",
         languages: "Java",
         tools: "IntelliJ IDEA, Bitbucket, Gradle, TeamCity, Manuscript",
         description: [
-            "Collaborating with a cross-functional team of software developers and UX/UI designers to implement UI tools and automated test suites using Java, IntelliJ IDEA, Gradle, Bitbucket, and TeamCity, streamlining feature development for the Globe Pulsed Field System, an advanced medical device technology designed to treat atrial fibrillation through high-definition cardiac mapping, single-shot pulmonary vein isolation, and atrial ablation."
+            "Collaborating with a cross functional team of software developers and UX/UI designers to build tools and automated test suites using Java, Gradle, IntelliJ IDEA, Bitbucket, and TeamCity, streamlining feature development for the Globe Pulsed Field System, an advanced medical technology designed to treat atrial fibrillation through high-definition cardiac mapping, single shot pulmonary vein isolation, and atrial ablation",
+            "Designing and developing a configuration management tool that parses and analyzes 3000+ Java source code files across multiple Gradle modules, using static analysis and symbol solving to perform abstract syntax tree parsing, type inference, and reference resolution for extracting, interpreting, and managing complex configuration data",
+            "Building an interactive interface supporting real time configuration modifications with integrated type checking and validation, improving the efficiency of cross-team software design, development, and testing workflows",
+            "Leveraging multithreading to parallelize parsing operations and implementing caching mechanisms to optimize data retrieval, eliminate redundant computations, and improve load times by over 83%"
+
         ],
         image: "/images/work/kardium.jpeg"
     }
