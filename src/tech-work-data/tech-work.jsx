2cd46f7143fa00aac28d12e123dbf4ab45a61af9
Stephanie Xue <stephaniexue3@gmail.com>
2025-11-04 21:06:04 -0800

    update resume and kardium description

--- src/tech-work-data/tech-work.jsx
+++ src/tech-work-data/tech-work.jsx
@@ -9,8 +9,8 @@ export default [
         tools: "IntelliJ IDEA, Bitbucket, Gradle, TeamCity, Manuscript",
         description: [
             "Collaborating with a cross functional team of software developers and UX/UI designers to build tools and automated test suites using Java, Gradle, IntelliJ IDEA, Bitbucket, and TeamCity, streamlining feature development for the Globe Pulsed Field System, an advanced medical technology designed to treat atrial fibrillation through high-definition cardiac mapping, single shot pulmonary vein isolation, and atrial ablation",
-            "Designing and developing a configuration management tool that parses and analyzes 3000+ Java source code files across multiple Gradle modules, using static analysis and symbol solving to perform abstract syntax tree parsing, type inference, and reference resolution for extracting, interpreting, and managing complex configuration data",
-            "Building an interactive interface supporting real time configuration modifications with integrated type checking and validation, improving the efficiency of cross-team software design, development, and testing workflows",
+            "Designing and developing a configuration management tool that parses and analyzes thousands of Java source code files in a large-scale multi-module Gradle codebase, using static analysis and symbol solving to perform abstract syntax tree parsing, type inference, and reference resolution for extracting, interpreting, and managing complex software configuration data",
+            "Building an interactive interface supporting real time configuration modifications with integrated type checking and validation, improving the efficiency of cross-team software design and development workflows",
             "Leveraging multithreading to parallelize parsing operations and implementing caching mechanisms to optimize data retrieval, eliminate redundant computations, and improve load times by over 83%"
 
         ],
