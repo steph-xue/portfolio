3412f4a01f14d58f4267db58536c88dac9f3cc19
Stephanie Xue <stephaniexue3@gmail.com>
2025-12-13 22:40:26 -0800

    update work experience

--- src/tech-work-data/tech-work.jsx
+++ src/tech-work-data/tech-work.jsx
@@ -9,8 +9,8 @@ export default [
         tools: "Gradle, JUnit, Launch4j, IntelliJ IDEA, Bitbucket, TeamCity, Manuscript",
         description: [
             "Collaborating with a cross functional team of software developers and UX/UI designers to build UI tools and automated test suites using Java, Gradle, JUnit, IntelliJ IDEA, Bitbucket, and TeamCity, streamlining feature development for the Globe Pulsed Field System, an advanced medical technology designed to treat atrial fibrillation through high-definition cardiac mapping, single shot pulmonary vein isolation, and atrial ablation",
-            "Designing and developing a configuration management tool that parses thousands of source code files in a large-scale multi-module Gradle codebase, using static analysis and symbol solving to perform abstract syntax tree parsing, type inference, and reference resolution for extracting, interpreting, and managing complex software configuration data",
-            "Building an interactive interface supporting real time configuration modifications with integrated type checking and validation, improving the efficiency of cross-team software design and development workflows",
+            "Designing and developing a configuration management tool that parses a large-scale multi-module Gradle codebase, leveraging static analysis and symbol solving to perform abstract syntax tree parsing, type inference, and reference resolution to successfully extract, interpret, and classify 98.6% of software configuration data",
+            "Building an interactive interface supporting real time configuration modifications with integrated type checking and validation logic, reducing configuration lookup time to seconds and reducing manual configuration errors by 95%",
             "Leveraging multithreading to parallelize parsing operations and implementing caching mechanisms to optimize data retrieval, eliminate redundant computations, and improve load times by over 83%",
             "Implementing Gradle and XML-based configuration modifications to support multi-module packaging, generating JAR artifacts and integrating Launch4j to create executable distributions"
 
