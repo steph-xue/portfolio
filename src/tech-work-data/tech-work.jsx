052af90fcb4c6a24b7e89a9601a9e7f17499175a
Stephanie Xue <stephaniexue3@gmail.com>
2025-12-18 23:14:44 -0800

    update work experience

--- src/tech-work-data/tech-work.jsx
+++ src/tech-work-data/tech-work.jsx
@@ -12,7 +12,7 @@ export default [
             "Designing and developing a configuration management tool that parses a large-scale multi-module Gradle codebase, leveraging static analysis and symbol solving to perform abstract syntax tree parsing, type inference, and reference resolution to successfully extract, interpret, and classify 98.6% of software configuration data",
             "Building an interactive interface supporting real time configuration modifications with integrated type checking and validation logic, reducing configuration lookup time to seconds and reducing manual configuration errors by 95%",
             "Leveraging multithreading to parallelize parsing operations and implementing caching mechanisms to optimize data retrieval, eliminate redundant computations, and improve load times by over 83%",
-            "Implementing Gradle, Launch4j (XML), and SCons SConscript configurations to generate and package JAR and Windows executable artifacts with multiple module dependencies into installers within a TeamCity CI pipeline, with Windows batch scripts to launch the packaged executables"
+            "Implementing Gradle, Launch4j (XML), and SCons configurations to build and package JAR and Windows executable artifacts with multiple module dependencies into installers within a TeamCity CI pipeline, with supporting Windows batch scripts to launch the packaged executables"
 
         ],
         image: "/images/work/kardium.jpeg"
